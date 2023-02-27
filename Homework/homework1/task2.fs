76bb76b66c50f160a7f2a0980d16c63f78ac6349
rinatisk <70975579+rinatisk@users.noreply.github.com>
2023-02-27 19:52:40 +0300

    feat: add first homework

---
+++ Homework/homework1/task2.fs
@@ -0,0 +1,12 @@
+﻿module Homework1.task2
+
+let rec fibonacciRecursive n current previous =
+    match n with
+    | 0 -> previous
+    | 1 -> current
+    | n -> fibonacciRecursive (n - 1) (current + previous) current
+
+let rec fibonacci n =
+    fibonacciRecursive n 1 1
+    
+printf "%i" <| fibonacci 6
---
+++ Homework/homework1/task3.fs
@@ -0,0 +1,11 @@
+﻿module Homework1.task3
+
+let rec reverseListRecursive currentList acc =
+    match currentList with
+    | [] ->  acc
+    | head::tail -> reverseListRecursive tail (head::acc)
+
+let rec reverseList list =
+    reverseListRecursive list []
+    
+printf "%A" <| reverseList [1 .. 10]
