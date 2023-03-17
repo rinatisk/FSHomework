d4ffe3744d1b26d2a34c5af079debb7cbdbb09d0
rinatisk <70975579+rinatisk@users.noreply.github.com>
2023-03-17 03:03:18 +0300

    feat: add 2.1 task

---
+++ Homework/Homework_2/Task1/Task1/Task1.fs
@@ -0,0 +1,12 @@
+﻿namespace Task1
+
+module Task1 =
+
+    let countEvenSumBy list =
+        list |> List.sumBy (fun element -> (abs element + 1) % 2)
+
+    let countEvenFilter list =
+        list |> List.filter (fun element -> element % 2 = 0) |> List.length
+
+    let countEvenFold list =
+        List.fold (fun acc element -> (abs element + 1) % 2 + acc) 0 list
