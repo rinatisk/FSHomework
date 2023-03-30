f4eae6388c49e7dd52c848daa3b107441799774a
rinatisk <70975579+rinatisk@users.noreply.github.com>
2023-03-31 02:01:09 +0300

    feat: add 4.1 task

---
+++ Homework/Homework_4/Task1/Task1/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let brackets = Map [('(',')');('[',']');('{','}')]
+let isCorrect (input: string) = 
+    let rec isCorrectRec leftPart rightPart =
+        match leftPart, rightPart with
+        | [], [] -> true
+        | _::_, [] -> false
+        | _, h::t when Map.keys brackets |> Seq.contains h -> isCorrectRec (h::leftPart) t
+        | h::t, h2::t2 when Map.values brackets |> Seq.contains h2 && Map.find h brackets = h2 -> isCorrectRec t t2
+        | _, h::_ when Map.values brackets |> Seq.contains h -> false
+        | _, _::t -> isCorrectRec leftPart t
+    isCorrectRec (Seq.toList input) []
