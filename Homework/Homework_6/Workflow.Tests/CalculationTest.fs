6e1dc1bf5b4c960d731659c5b3d1237d6e17e4d2
rinatisk <70975579+rinatisk@users.noreply.github.com>
2023-04-28 04:14:45 +0300

    feat: add tests for 6.1

---
+++ Homework/Homework_6/Workflow.Tests/CalculationTest.fs
@@ -0,0 +1,25 @@
+﻿module Workflow.Tests.CalculationTest
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+
+let calculate = Calculating.CalculateBuilder()
+
+[<Test>]
+let ReturnTest () =
+    Check.QuickThrowOnFailure (fun s ->
+        let returned = calculate {
+            return s
+        }
+        System.Int32.Parse returned = s)
+
+
+[<Test>]
+let BindTest () =    
+    Check.QuickThrowOnFailure (fun x ->
+        let returned = calculate {
+            let! i = x 
+            return i
+        }
+        returned = x)
---
+++ Homework/Homework_6/Workflow.Tests/RoundingTest.fs
@@ -0,0 +1,22 @@
+﻿module Workflow.Test.RoundingTests
+
+open NUnit.Framework
+open FsUnit
+
+open Rounding
+
+let rounding = RoundingBuilder
+
+
+[<TestCase(0.0)>]
+[<TestCase(1.0)>]
+[<TestCase(-21.43)>]
+[<TestCase(System.Double.NaN)>]
+[<TestCase(System.Double.PositiveInfinity)>]
+[<TestCase(System.Double.NegativeInfinity)>]
+let RoundTest x =    
+        let returned = rounding 3 {
+            let! y = x 
+            return y
+        }
+        returned |> should equal x
