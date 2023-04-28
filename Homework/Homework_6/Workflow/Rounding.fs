85ef26cc06792bfe2c5d459a7675392c4d4cb460
rinatisk <70975579+rinatisk@users.noreply.github.com>
2023-04-28 04:19:40 +0300

    feat: add 6.1

---
+++ Homework/Homework_6/Workflow/Rounding.fs
@@ -0,0 +1,8 @@
+﻿module Rounding
+
+type RoundingBuilder(digits : int) =
+    member this.Bind (value : float, f) =
+        (value, digits) |> System.Math.Round |> f
+    member this.Return (value : float) =
+        System.Math.Round (value, digits)
+        
