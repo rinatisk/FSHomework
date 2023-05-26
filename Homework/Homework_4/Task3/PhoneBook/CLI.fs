91d47eef5e84861bf6c78a0f7159fe0ac15bb760
rinatisk <70975579+rinatisk@users.noreply.github.com>
2023-05-26 03:58:41 +0300

    feat: add solution for 4.3

---
+++ Homework/Homework_4/Task3/PhoneBook/CLI.fs
@@ -0,0 +1,70 @@
+﻿module PhoneBook.CLI
+
+open System
+open PhoneBook
+open System.IO
+
+let readNumbersFromFile file phoneBook =
+    let newNumbers = File.ReadAllLines(file) |> Array.toList |> List.map (fun s -> ((s.Split ":")[0], (s.Split ":")[1]))
+    Map.ofList ((phoneBook |> getAllRecords) @ newNumbers)
+
+let writeNumbersToFile file phoneBook =
+    let numbers = phoneBook |> getAllRecords |> List.map(fun (k, v) -> k + v)
+    File.WriteAllLines(file, numbers)
+
+let printBook phoneBook =
+    phoneBook |> getAllRecords |> List.map (fun (k, v) -> k + v) |> List.map(fun s -> printf $"{s}")
+
+let main() =
+    printfn "
+             0 /// exit
+             1 /// Add *name* *phone*
+             2 /// ReadFromFile *file*
+             3 ///  WriteToFile *filename*
+             4 /// GetNumber *name*
+             5 /// GetPhone *number*
+             6 /// PrintAllRecords"
+
+    let phoneBook = initPhoneBook()
+
+    let rec executionLoop phoneBook =
+        let args = Console.ReadLine().Split " "
+        let command = args[0]
+        match command with
+            | "0" -> true
+            | "1" ->
+                let name = args[1]
+                let phone = args[2]
+                addNumber name phone phoneBook |> ignore
+                executionLoop phoneBook
+            | "2" ->
+                let file = args[1]
+                readNumbersFromFile file phoneBook |> ignore
+                executionLoop phoneBook
+            | "3" ->
+                let file = args[1]
+                writeNumbersToFile file phoneBook
+                executionLoop phoneBook
+            | "4" ->
+                let name = args[1]
+                let number = getNumberByName name phoneBook
+                match number with
+                | Some number -> printf $"Phone:{number}"
+                | None -> printf "No phone in phoneBook"
+                executionLoop phoneBook
+            | "5" ->
+                let phone = args[1]
+                let name = getNameByNumber phone phoneBook
+                match name with
+                | Some name -> printf $"Name:{name}"
+                | None -> printf "No name in phoneBook"
+                executionLoop phoneBook
+            | "6" ->
+                printBook phoneBook |> ignore
+                executionLoop phoneBook
+            | _ ->
+                executionLoop phoneBook
+
+    executionLoop phoneBook
+
+main() |> ignore
---
+++ Homework/Homework_4/Task3/PhoneBook/Program.fs
@@ -0,0 +1,17 @@
+﻿module PhoneBook
+
+let initPhoneBook() =
+    Map.empty<string, string>
+
+let addNumber name number phoneBook =
+    Map.add name number phoneBook
+
+let getNumberByName name phoneBook =
+    Map.tryFind name phoneBook
+
+let getNameByNumber number phoneBook =
+    Map.tryFindKey (fun _ v -> v = number) phoneBook
+
+let getAllRecords phoneNumber =
+    Map.toList phoneNumber
+    
